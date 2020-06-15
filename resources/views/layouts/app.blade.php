4816a23b962953e43c26ae433fdf2e97208d851f
robertgarciajr <robgjr86@gmail.com>
2020-06-14 19:28:47 -0700

    Made Layouts Responsive for Mobile and Tablets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,9 @@
     <body>
         <section id="app-layout">
             @include('includes.side-menu')
+            <section id="content-section">
             @yield('content')
+            </section>
         </section>
     </section>
     </body>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-   <div class="welcome-jumbo">
-      <h1>About Page</h1>  
+   <div id="about-page">
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>About Page</h1> 
+                    <p>I'm baby skateboard synth mustache brooklyn thundercats snackwave lumbersexual flannel banh mi pork belly distillery letterpress literally shaman. Gastropub YOLO street art, cray banjo vice health goth hashtag DIY skateboard activated charcoal church-key. Aesthetic chillwave squid, vice letterpress pop-up organic occupy cred. Typewriter salvia semiotics tumeric hoodie. Tilde plaid pabst migas YOLO whatever pok pok taiyaki austin master cleanse prism church-key helvetica hammock 90's.
+                    </p>
+                    <p>I'm baby skateboard synth mustache brooklyn thundercats snackwave lumbersexual flannel banh mi pork belly distillery letterpress literally shaman. Gastropub YOLO street art, cray banjo vice health goth hashtag DIY skateboard activated charcoal church-key. Aesthetic chillwave squid, vice letterpress pop-up organic occupy cred. Typewriter salvia semiotics tumeric hoodie. Tilde plaid pabst migas YOLO whatever pok pok taiyaki austin master cleanse prism church-key helvetica hammock 90's.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="/img/about-page-img.jpeg" class="img-fluid">
+                </div>
+            </div>
+            
+        </div> 
     </div>
 @endsection

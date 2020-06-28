532c9066829970610be0e1c2822666e4ca6f156b
robertgarciajr <robgjr86@gmail.com>
2020-06-27 18:40:55 -0700

    added food categories page html and css

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
+Route::get('/admin', function () {
+    return view('admin/dashboard');
+});
+Route::get('/admin/food-categories', function () {
+    return view('admin/food-categories/all');
+});
+Route::get('/admin/register', function () {
+    return view('admin/register');
+});
+Route::get('/admin/login', function () {
+    return view('admin/login');
+});
 Route::get('/menu/{slug}', function () {
     return view('menu/single-menu');
 });
